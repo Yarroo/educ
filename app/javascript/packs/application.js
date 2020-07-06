25911f501bfc5ae30cc9916fca3d71f5bd4c6cc9
Sergey Vershinin <accountwm@gmail.com>
2020-07-06 23:43:39 +0300

    webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import 'bootstrap';
-import './stylesheets/application.scss';
+import 'jquery'
+import 'src/plugins'
+import 'stylesheets/site'
 
+Rails.start()
