f3c2231de45fb286947c8406de6a3f007ab38062
Sergey Vershinin <accountwm@gmail.com>
2020-07-14 08:28:27 +0300

    возможность редактирования

--- app/admin/schools.rb
+++ app/admin/schools.rb
@@ -8,6 +8,10 @@ ActiveAdmin.register School do
 
   filter :city, collection: proc {City.includes(:schools).where.not(schools: {id: nil}).pluck(:name, :id).uniq}
 
+  permit_params :name, :short_name, :director, :address, :phone,
+                :site, :city_id, email: []
+
+
   index do
     selectable_column
     id_column
@@ -32,5 +36,13 @@ ActiveAdmin.register School do
     column (:email) { |school| school.email.join(", ") }
     column (:phone) { |school| school.phone.join(", ")}
     column :site
+  end
+
+  controller do
+    def update
+      email = params[:school][:email].split(",")
+      params[:school][:email] = Array.wrap(email)
+      super
+    end
   end
 end
