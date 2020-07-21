21ca885a900866dbf79cf7e277eb6a1f8df053f6
Sergey Vershinin <accountwm@gmail.com>
2020-07-21 23:15:05 +0300

    рефакторинг

--- lib/csv_scrubber.rb
+++ lib/csv_scrubber.rb
@@ -4,25 +4,34 @@ class CsvScrubber < SchoolScrubber
   end
 
   def scrub
+    delete_empty_row
+    scrub_rows
+    set_city_and_region
+    @importer.batch_slice_columns(['name', 'short_name', 'address', 'director', 'phone', 'email', 'site', :city_id ])
+  end
+
+  def delete_empty_row
     @importer.csv_lines = @importer.csv_lines.select { |line| line.compact.present?}
+  end
 
-    headers = @importer.headers
-
-    headers.each do |header|
+  def scrub_rows
+    @importer.headers.each do |header|
       next if header[1] == :city_id || header[1] == "region"
+
       values = @importer.values_at(header[1])
       scrub_value = values.reduce({}) do |memo, value|
         memo.merge(value => send("scrub_#{header[1]}", value))
       end
+
       @importer.batch_replace(header[1], scrub_value)
     end
+  end
 
+  def set_city_and_region
     cities_values = @importer.values_at(:city_id)
-    region_values = headers.map(&:last).include?("region") ? @importer.values_at("region") : []
+    region_values = @importer.headers.map(&:last).include?("region") ? @importer.values_at("region") : []
 
-    values = cities_values.map.with_index do |city, index|
-      [city, region_values[index]]
-    end
+    values = cities_values.map.with_index { |city, index| [city, region_values[index]]}
 
     cities = values.reduce({}) do |memo, value|
       region_name = value[1]
@@ -37,7 +46,6 @@ class CsvScrubber < SchoolScrubber
     end
 
     @importer.batch_replace(:city_id, cities)
-    @importer.batch_slice_columns(['name', 'short_name', 'address', 'director', 'phone', 'email', 'site', :city_id ])
   end
 
   def scrub_email(text)
